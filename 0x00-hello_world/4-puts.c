cb95b03276dd2a8d985a8b354cbbc0b8654e14b2
kvngstar <kvngsley019@gmail.com>
2023-08-10 19:49:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - I am learning C, never saw it comming
+ * Return:0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
