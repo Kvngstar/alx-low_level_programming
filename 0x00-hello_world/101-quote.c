5dde73bf38a8096b13d2f01427eff8f6cf29c7f3
kvngstar <kvngsley019@gmail.com>
2023-08-10 21:04:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return - Always 1 (Sucess)
- * /
+ * Return: Always 1 (Sucess)
+ */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
