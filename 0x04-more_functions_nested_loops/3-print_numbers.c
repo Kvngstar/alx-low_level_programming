fbfe2677f51e2125b8d159f17fbe6c5e09379eb5
kvngstar <kvngsley019@gmail.com>
2023-08-17 17:58:15 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - Prints from 0 to 9
- * Return: 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (x = 0; x <= 9, x++)
 	{
-		_putchar(x + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
