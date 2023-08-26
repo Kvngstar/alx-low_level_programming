27ccfe23abdeb13f133d5e8928d4880e4e60c771
kvngstar <kvngsley019@gmail.com>
2023-08-25 19:40:54 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -27,7 +27,7 @@ void rev_string(char *n)
 }
 
 /**
- * infinite_add - add 2 numbers together
+ * *infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
